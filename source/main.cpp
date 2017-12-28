860aab0ee8fd2287fb21ef055986cb44a06309bf
NikAlex <alez90@yandex.ru>
2017-12-28 05:45:38 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
 	Rubik rubik;
-	char buff[56] = { "" };
+	char buff[55] = { "" };
 	vector<char> solution;
 	ifstream fin;
 	ofstream fout;
@@ -14,22 +14,24 @@ int main(int argc, char* argv[]) {
 		fout << "error";
 		fin.close();
 		fout.close();
+		system("pause");
 		return 0;
 	}
-
 	fin.close();
-	rubik.readRubik(buff);
+	rubik.getBuff(buff);
+	rubik.readRubik(); 
 	rubik.solve(solution);
 
 	if (rubik.isSolved() == false) {
 		fout << "error";
 		fout.close();
+		system("pause");
 		return 0;
 	}
 
 	for (int i = 0; i < solution.size(); i++) {
 		fout << solution[i];
 	}
-
+	
 	return 0;
 }
