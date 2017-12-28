ba888efd08c42fee17cd56eb12827a98fdd69b0c
NikAlex <alez90@yandex.ru>
2017-12-28 05:45:09 +0300

    Update header.hpp

--- source/header.hpp
+++ source/header.hpp
@@ -12,27 +12,25 @@ using namespace std;
 
 
 class Rubik {
-private:
-	
-	char Buff[55];
-	char *Cube[6][9];
-	int searchEdge(const char& ar, const char& ab) const;
-	int searchCorner(const char& a, const char& iz, const char& de) const;
-	void sequence(const string& sec, vector<char>& solution);
-	void whiteCross(vector<char>& solution);
-	void cornersFirstStep(vector<char>& solution);
-	void secondStep(vector<char>& solution);
-	void yellowCross(vector<char>& solution);
-	void permutationFinalCorners(vector<char>& solution);
-	void positionFinalCorners(vector<char>& solution);
+	private:
+		char Buff[55];
+		char *Cube[6][9];
+		int searchEdge(const char& ar, const char& ab) const;
+		int searchCorner(const char& a, const char& iz, const char& de) const;
+		void sequence(const string& sec, vector<char>& solution);
+		void whiteCross(vector<char>& solution);
+		void cornersFirstStep(vector<char>& solution);
+		void secondStep(vector<char>& solution);
+		void yellowCross(vector<char>& solution);
+		void permutationFinalCorners(vector<char>& solution);
+		void positionFinalCorners(vector<char>& solution);
 
-public:
-
-	void getBuff(char(&buff)[55]);
-	bool isSolved() const;
-	void Rotate(vector<char>& solution, char& side);
-	void solve(vector<char>& solution);
-	void readRubik();
+	public:
+		void getBuff(char(&buff)[55]);
+		bool isSolved() const;
+		void Rotate(vector<char>& solution, char& side);
+		void solve(vector<char>& solution);
+		void readRubik();
 };
 
-bool test(std::istream& is, char(*buff));
+bool test(istream& is, char(*buff));
